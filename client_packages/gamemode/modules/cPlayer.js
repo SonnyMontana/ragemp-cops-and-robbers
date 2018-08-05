ef5eff1f1301e2db113dd10ae72cc577dc50a1f4
SonnyMontana <42115895+SonnyMontana@users.noreply.github.com>
2018-08-05 16:23:27 +0200

    Add files via upload

---
+++ client_packages/gamemode/modules/cPlayer.js
@@ -0,0 +1,286 @@
+
+
+const hudComponentID = 19;
+const rankBarColor = 116;
+const timerBarLib = require("/gamemode/modules/lib/timerbars")
+const misc = require('/gamemode/cMisc.js');
+const NativeUI = require('/gamemode/modules/lib/nativeui');
+
+var player = mp.players.local;
+let rewardBar = null;
+let robberyBar = null;
+let interval = null;
+
+mp.events.add({
+    "setWaypoint": (x, y) => {
+        mp.game.ui.setNewWaypoint(x, y);
+    },
+    "fadeOut": () => {
+        mp.game.cam.doScreenFadeOut(500);
+    },
+    "fadeIn": () => {
+        mp.game.cam.doScreenFadeIn(1000);
+    },
+    "InterruptRobberyTimer": () => {
+        if(interval) {
+            clearInterval(interval);
+            interval = null;
+        }
+        robberyBar.visible = false;
+        robberyBar = null;
+
+        rewardBar.visible = false;
+        rewardBar = null;
+    },
+    "setRobberyTimer": (time) => {
+        timeLeft = time;
+        mp.events.call("ShowShardMessage", "Robbery started!", "Stay in the checkpoint to not interrupt it!", 0, 6);
+        if(robberyBar == null) {
+            robberyBar = new timerBarLib.TimerBar("TIME LEFT");  
+            robberyBar.text = `${time}`;
+        }
+        interval = setInterval(function() {
+            if(timeLeft == 0) {
+                interval = clearInterval(interval);
+                interval = null;
+            }  else {
+                timeLeft -= 1;  
+                if(timeLeft % 2 === 0) {
+                    robberyBar.textColor = [183, 0, 0, 255];
+                } else {
+                    robberyBar.textColor = [255, 255, 255, 255];
+                }  
+            }
+            robberyBar.text = (timeLeft != 0) ? `${timeLeft}` : `Done`;  
+        }, 1000);
+    },
+    "clearRobberyTimers": () => {
+        robberyBar.visible = false;
+        robberyBar = null;
+        rewardBar.visible = false;
+        return rewardBar = null;
+
+    },
+    "setRewardBar": (reward) => {
+        if(rewardBar == null) { 
+            rewardBar = new timerBarLib.TimerBar("BENEFITS");
+            rewardBar.textColor = [0, 200, 0, 255];
+        }
+        if(reward != 0) {            
+            rewardBar.text = `$${reward}`;
+        }
+    },
+    "cJobSelection": () => {
+        mp.gui.chat.show(false);
+
+
+        ui = new NativeUI.Menu("Select a job", "Select a job as Civil!", new NativeUI.Point(50,200));
+        item = new NativeUI.UIMenuItem(
+            "Hacker",
+            "Hack stuff, little or big!"
+        );
+        item.SetLeftBadge(4);
+        ui.AddItem(item);
+
+        ui.ItemSelect.on(item => {
+            switch(item.Text) {
+                case "Hacker":
+                    jobID = 0;
+                    break;
+            }
+            mp.events.callRemote("sJobSelection", jobID);
+            ui.Close();
+            ui = null;
+            mp.gui.chat.show(true);
+
+        });
+    },
+    "playSound": (SoundName, SoundSetName) => {
+        mp.game.audio.playSoundFrontend(1, SoundName, SoundSetName, true);
+    },
+
+    
+    "ATMChoice": (id) => {
+        mp.gui.chat.show(false);
+
+
+        ui = new NativeUI.Menu("ATM", "Select what you want to do!", new NativeUI.Point(50,200));
+        item = new NativeUI.UIMenuItem(
+            "ATM Menu",
+            "WithDraw / Deposit money"
+        );
+        ui.AddItem(item);
+        item = new NativeUI.UIMenuItem(
+            "Hack",
+            "You need to be hacker for this."
+        );
+        ui.AddItem(item);
+        item = new NativeUI.UIMenuItem(
+            "Exit",
+            "Exit, is exit, whatever it is !"
+        );
+        ui.AddItem(item);
+        ui.ItemSelect.on(item => {
+            switch(item.Text) {
+                case "ATM Menu":
+                    mp.events.callRemote("sAction", 0);
+                    break;
+                case "Hack":
+                    mp.events.callRemote("sAction", 1, id);
+                    break;
+            }
+            ui.Close();
+            ui = null;
+            mp.gui.chat.show(true);
+        });
+    },    
+    "AMMUNATIONchoice": (id) => {
+        mp.gui.chat.show(false);
+
+
+        ui = new NativeUI.Menu("Ammunation", "Select what you want to do!", new NativeUI.Point(50,200));
+        item = new NativeUI.UIMenuItem(
+            "Buy weapons",
+            ""
+        );
+        ui.AddItem(item);
+        item = new NativeUI.UIMenuItem(
+            "Hold up!",
+            "Steal money!"
+        );
+        ui.AddItem(item);
+        item = new NativeUI.UIMenuItem(
+            "Exit",
+            "Exit, is exit, whatever it is !"
+        );
+        ui.AddItem(item);
+        ui.ItemSelect.on(item => {
+            switch(item.Text) {
+                case "Buy weapons":
+                    break;
+                case "Hold up!":
+                    mp.events.callRemote("sAction", 2, id);
+                    break;
+            }
+            ui.Close();
+            ui = null;
+            mp.gui.chat.show(true);
+        });
+    },    
+    "247choice": (id) => {
+        mp.gui.chat.show(false);
+
+
+        ui = new NativeUI.Menu("24/7", "Select what you want to do!", new NativeUI.Point(50,200));
+        item = new NativeUI.UIMenuItem(
+            "Buy stuff",
+            ""
+        );
+        ui.AddItem(item);
+        item = new NativeUI.UIMenuItem(
+            "Hold up!",
+            "Steal money!"
+        );
+        ui.AddItem(item);
+        item = new NativeUI.UIMenuItem(
+            "Exit",
+            "Exit, is exit, whatever it is !"
+        );
+        ui.AddItem(item);
+        ui.ItemSelect.on(item => {
+            switch(item.Text) {
+                case "Buy stuff":
+                    ui.Close();
+                    mp.events.call("247ItemsChoice");
+                    break;
+                case "Hold up!":
+                    mp.events.callRemote("sAction", 2, id);
+                    break;
+                default:
+                    ui.Close();
+                    break;
+            }
+        });
+        ui.MenuClose.on( () => {
+            mp.gui.chat.show(true);
+            ui = null;
+        })
+    },
+    "247ItemsChoice": () => {
+        mp.gui.chat.show(false);
+        ui = new NativeUI.Menu("24/7", "Select what you want to do!", new NativeUI.Point(50,200));
+        items = {
+            "Apple": {name:"Apple",desc:"Nice to get some health more!",price:"20",health:10},
+            "Chips": {name:"Chips",desc:"May make you fat!",price:"30",health:20},
+            "Bread": {name:"Bread",desc:"Bread, it's good, it's french, as the developer is!",price:"50",health:30}
+        }
+        for(var key in items) {
+            item = items[key];
+            fitem = new NativeUI.UIMenuItem(item.name, item.desc);
+            fitem.SetRightLabel("~g~$" + item.price);
+            ui.AddItem( fitem );
+        }
+        ui.ItemSelect.on ( (item, index) => {
+            for(var key in items) {
+                if(item.Text != key) continue;
+                mp.events.callRemote("sAction", 3, item.Text, items[key].price, items[key].health);
+                ui.Close();
+            }
+        });
+        ui.MenuClose.on( () => {
+            mp.gui.chat.show(true);
+            ui = null;
+        });
+
+    },
+
+
+    // HACKERS
+    "hackerConsole": (type ) => {     
+        mp.players.local.inMenu = true;
+        misc.prepareToCef();
+        player.freezePosition(true);
+        mp.game.ui.displayHud(false);
+        misc.openCef('package://gamemode/browser/player/hacker/hacker.html');
+        switch(type) {
+            case "ATM":
+                misc.injectCef("setHackingType", "ATM")
+        }
+    },
+    "hackerConsoleClose": () => {
+        misc.closeCef();
+        mp.players.local.inMenu = false;
+        player.freezePosition(false);
+        mp.game.ui.displayHud(true);
+    },
+    "hackerATMWithdraw": () => {
+        mp.events.call("hackerConsoleClose");
+        mp.events.callRemote("hackerATMWithdraw", mp.players.local);
+    },
+    "updateRankBar": (limit, nextLimit, previousXP) => {
+        if (!mp.game.graphics.hasHudScaleformLoaded(hudComponentID)) {
+            mp.game.graphics.requestHudScaleform(hudComponentID);
+            while (!mp.game.graphics.hasHudScaleformLoaded(hudComponentID)) mp.game.wait(0);
+    
+            mp.game.graphics.pushScaleformMovieFunctionFromHudComponent(hudComponentID, "SET_COLOUR");
+            mp.game.graphics.pushScaleformMovieFunctionParameterInt(rankBarColor);
+            mp.game.graphics.popScaleformMovieFunctionVoid();
+        }
+    
+        mp.game.graphics.pushScaleformMovieFunctionFromHudComponent(hudComponentID, "SET_RANK_SCORES");
+        mp.game.graphics.pushScaleformMovieFunctionParameterInt(limit);
+        mp.game.graphics.pushScaleformMovieFunctionParameterInt(nextLimit);
+        mp.game.graphics.pushScaleformMovieFunctionParameterInt(previousXP);
+        mp.game.graphics.pushScaleformMovieFunctionParameterInt(player.getVariable("currentXP"));
+        mp.game.graphics.pushScaleformMovieFunctionParameterInt(player.getVariable("currentLevel"));
+        mp.game.graphics.popScaleformMovieFunctionVoid();
+    },
+    "playerDead": () => {
+        mp.game.gameplay.setFadeOutAfterDeath(true);
+        setTimeout(function() {
+            mp.game.cam.doScreenFadeIn(1000);
+        }, 10000)
+    }
+
+
+}); 
---
+++ client_packages/gamemode/modules/cVehicle.js
@@ -0,0 +1,430 @@
+const timeBarLib = require('/gamemode/modules/lib/timerbars');
+const misc = require('/gamemode/cMisc.js');
+const NativeUI = require('/gamemode/modules/lib/nativeui');
+const Menu = NativeUI.Menu;
+const UIMenuItem = NativeUI.UIMenuItem;
+const UIMenuListItem = NativeUI.UIMenuListItem;
+const UIMenuCheckboxItem = NativeUI.UIMenuCheckboxItem;
+const UIMenuSliderItem = NativeUI.UIMenuSliderItem;
+const BadgeStyle = NativeUI.BadgeStyle;
+const Point = NativeUI.Point;
+const ItemsCollection = NativeUI.ItemsCollection;
+const Color = NativeUI.Color;
+const ListItem = NativeUI.ListItem;
+
+
+// VEHICLE CUSTOM
+var price = 0;
+var ui = null;
+var uiType = null;
+
+var timeBar = null;
+
+var vehHandle = null;
+var playerVeh = null;
+var vehPrices = null;
+var vehP = 0;
+var vehS = 0;
+
+var vehDefaultP = 0;
+var vehDefaultS = 0;
+
+var modIndex = {
+    0: "Spoilers",
+    1: "Front Bumper",
+    2: "Rear Bumper",
+    3: "Side Skirt",
+    4: "Exhaust",
+    5: "Frame",
+    6: "Grille",
+    7: "Hood",
+    8: "Fender",
+    9: "Right Fender",
+    10: "Roof",
+    11: "Engine",
+    12: "Brakes",
+    13: "Transmission",
+    14: "Horns",
+    15: "Suspension",
+    16: "Armor",
+    18: "Turbo",
+    22: "Headlights",
+    23: "Front Wheels",
+    20: "Util Shadow Silver",
+    24: "Black Wheels",
+    25: "Plate Holders",
+    26: "Vanity Plates",
+    27: "Trim Design",
+    28: "Ornaments",
+    29: "Dashboard",
+    30: "Dial Design",
+    31: "Door Speaker",
+    32: "Seats",
+    33: "Steering Wheel",
+    34: "Shift Lever",
+    35: "Plaques",
+    36: "Speakers",
+    37: "Trunk",
+    38: "Hydraulics",
+    39: "Engine Block",
+    40: "Air filter",
+    41: "Struts",
+    42: "Arch Cover",
+    43: "Aerials",
+    44: "Trim",
+    45: "Tank",
+    46: "Windows",
+    48: "Livery",
+    62: "Plate",
+    66: "Primary Color",
+    67: "Secondary Color",
+    69: "Window Tint",
+}
+var edits = {}
+
+function exitCustom() {
+    for(var key in edits) {
+        playerVeh.setMod(parseInt(key), -1);
+    }
+    playerVeh.setColours(vehDefaultP, vehDefaultS);
+
+    timeBar.visible = false;
+    timeBar = null;
+
+    price = 0; 
+    ui = null;
+    uiType = null;
+
+    vehHandle = null;
+    playerVeh = null;
+    vehPrices = null;
+    vehP = 0;
+    vehS = 0;
+
+    vehDefaultP = 0;
+    vehDefaultS = 0;
+
+   
+}
+
+
+function putInDefaultCam() {
+    if(misc.camExist) misc.destroyCam();    
+    misc.createCam(-340.07, -135.14, 39, 0, 0, 121, 50);
+}
+
+function addEditedItem(modType, modIndex) {
+    if(modIndex == -1) return delete edits[parseInt(modType)];
+    edits[parseInt(modType)] = parseInt(modIndex);
+    price = updatePrice();
+}
+
+function updatePrice() {
+    if(!timeBar) {
+        timeBar = new timeBarLib.TimerBar("Price");
+        timeBar.textColor = [114, 0, 0, 255];
+    }
+    let price = 0;
+    for(var key in edits) {
+        price += vehPrices[key][0] ? (vehPrices[key][parseInt(edits[key])] ? vehPrices[key][parseInt(edits[key])] : vehPrices[key][0]) : 0;    
+    }
+    timeBar.text = `$${price}`;
+    return price;
+}
+
+function createMenu() {    
+    if(ui) { ui.Close();  }
+    ui = new Menu(vehHandle.displayName, "Custom your car !", new Point(50, 50));
+}
+function showMainMenu() {
+    uiType = "home";
+    createMenu();
+    vehHandle.mods[66] = true;
+    vehHandle.mods[67] = true;
+    for(var key in vehHandle.mods) {        
+        if(typeof modIndex[key] == "undefined") continue;
+        item = new UIMenuItem(
+            modIndex[key]
+        );
+        if(typeof edits[key] != "undefined") {
+            if(typeof vehPrices[key][parseInt( edits[key] ) ] != "undefined" ) {
+                item.SetRightLabel("$" + vehPrices[key][parseInt( edits[key] )]);
+            } else {   
+                if(typeof vehPrices[key][0] != "undefined" ) {
+                    item.SetRightLabel("$" + vehPrices[key][0]);
+                } else {
+                    item.SetRightLabel("Unknown price" );
+                }
+
+            }
+        } 
+        ui.AddItem(item);
+    }
+
+    item = new UIMenuItem(
+        "Save custom",
+        "Argh! This action will make you lose money!"
+    );
+    ui.AddItem(item);
+    ui.ItemSelect.on(item => {
+        if (item instanceof UIMenuItem) {
+            if(uiType == "home" && item.Text != "Save custom") {
+                for(var key in vehHandle.mods){
+                    if(modIndex[key] == item.Text) {
+                        showSpecificMenu(key, item.Text);
+                    }
+                }                        
+            } else if(item.Text == "Save custom") {
+                uiType = "buyit";
+                mp.events.call("payVehicleCustom", playerVeh, price);
+                
+            }                   
+        }
+    });
+    ui.MenuClose.on(() => {
+        if(uiType == "home") {
+            mp.events.call("exitVehicleCustom");
+        } 
+    })
+}
+
+function showSpecificMenu(type, name) {
+    uiType = type;
+    createMenu();
+    ui.AddItem(new UIMenuItem(
+        "Back to menu"
+    ));
+    ui.AddItem(new UIMenuItem(
+        "Default " + name
+    ));
+
+    switch(parseInt(uiType)) {
+        case 62:
+        case 0:
+        case 4:
+        case 2:
+            misc.destroyCam();
+            misc.createCam(-348.388, -137.387, 39, 0, 0, 270.4, 50);
+            break;
+        case 15:
+        case 23:
+            misc.destroyCam();
+            misc.createCam(-341.156, -140.48, 39.028, 0, 0, 30.910, 50);
+            mp.players.local.heading = -30;
+            break;
+        case 66:
+        case 67:
+            misc.destroyCam();
+            misc.createCam(-339.07, -137.44, 40, -20, 0, 84.7, 50);
+            break;
+    }
+    if(parseInt(uiType) != 66 && parseInt(uiType) != 67) {
+        for(var i=0;i< vehHandle.mods[type].amount; i++) {
+            item = new UIMenuItem(
+                name + " " + String(parseInt(i) + 1),
+                "Press enter to preview & select the item!"
+            );
+            if(vehPrices[uiType][parseInt(i)]) {
+                item.SetRightLabel("$" + vehPrices[uiType][parseInt(i)])
+            } else {
+                item.SetRightLabel("Unknown price" );
+            }
+            ui.AddItem(item);
+        }
+    } else {
+        arraytomax = [];
+        for(var i=0;i<160;i++) { arraytomax.push(i); }
+        if(parseInt(uiType) == 66) {
+            ui.AddItem( new UIMenuSliderItem( name, arraytomax, vehDefaultP, "Change primary color") );
+        } else {
+            ui.AddItem( new UIMenuSliderItem( name, arraytomax, vehDefaultS, "Change secondary color") );
+        }
+        
+    }
+
+    ui.ItemSelect.on(item => {
+        if (item instanceof UIMenuItem) {
+            if(item.Text == "Back to menu") {
+                ui.Close();
+            }
+            else if(item.Text == "Default " + name) {
+                playerVeh.setMod(parseInt(uiType), -1);
+                addEditedItem(uiType, -1);
+            } else {
+                addEditedItem(uiType, parseInt( (item.Text).slice(-1) ) - 1)
+                playerVeh.setMod(parseInt(uiType), edits[uiType]); 
+            }
+            switch(parseInt(uiType)) {
+                case 22:                        
+                    playerVeh.setLights(3);
+                    setTimeout(function() { playerVeh.setLights(1); }, 5000);
+                    break;
+                case 14:
+                    playerVeh.startHorn(10000, 0, true);
+                    break;
+            }
+        }
+    });
+    ui.SliderChange.on((item, index, value) => {
+        if(parseInt(uiType) == 66) {
+            vehP = parseInt(value);
+        }
+        if(parseInt(uiType) == 67) {
+            vehS = parseInt(value);
+        }     
+        addEditedItem(uiType, parseInt(value))
+        playerVeh.setColours(vehP, vehS);
+
+    });
+    ui.MenuClose.on(() => {
+        if(uiType != "home") {
+            showMainMenu();
+            putInDefaultCam();
+        }
+    });
+}
+
+mp.events.add({
+    "showVehicleCustomHUD": (vehData, vehicle, prices) => {
+        if(vehData) {
+
+            mp.game.audio.playSound(-1, "Garage_Open", "CAR_STEAL_2_SOUNDSET", true, 0, true);
+
+            putInDefaultCam();
+            mp.game.ui.displayRadar(false);
+            mp.gui.chat.show(false);      
+            player.freezePosition(true);
+        
+            vehHandle = vehData;
+            playerVeh = vehicle;
+            vehPrices = prices;
+
+            colors = vehicle.getColours(vehDefaultP, vehDefaultS);
+            vehDefaultP = colors.colorPrimary;
+            vehDefaultS = colors.colorSecondary
+
+
+            playerVeh.setEngineOn(true, true, false);
+            
+            showMainMenu();
+
+        }
+    },
+    "exitVehicleCustom": () => {
+        setTimeout(function() { mp.events.callRemote("playerExitVehicleCustom", playerVeh); exitCustom(); }, 1000);
+        mp.events.call("fadeOut");
+        mp.game.ui.displayRadar(true);
+        mp.gui.chat.show(true);
+        player.freezePosition(false);
+        misc.destroyCam();
+        if(ui) { ui.Close(); ui = null; }
+    },
+    "payVehicleCustom": (vehicle, price) => {
+        mp.events.callRemote('vehicleApplyVehicleCustom', vehicle, JSON.stringify(edits));
+        mp.events.callRemote('playerPayVehicleCustom', price);
+    }
+});
+
+// CAR SHOP
+var cam = null;
+var type = null;
+var carShopType, carShopData, carShopPos;
+function carShopMenu() {   
+    data = carShopData; 
+    type = carShopType;
+    if(data.length == 1) return carShopClassMenu(0);
+    ui = new Menu(type, "", new Point(50, 50));  
+    for(var i = 0; i < data.length; i++) {
+        item = new UIMenuItem(
+            data[i].name
+        );
+        ui.AddItem(item);
+    }
+    let closing = true;
+    ui.IndexChange.on( (index) => {
+        mp.players.local.vehicleShop.model = data[index][Object.keys(data[index])[1]].hash;
+    });
+    ui.ItemSelect.on( (item, index) => {
+        closing = false;
+        ui.Close();
+        ui = null;
+        carShopClassMenu(index);
+    });
+    ui.MenuClose.on( () => {
+        if(closing) {
+            ui = null;
+            exitCarShop();  
+        }
+    });
+}
+function carShopClassMenu(id) {
+    data = carShopData[id];
+    ui = new Menu(data.name, "", new Point(50, 50));
+    for(var key in data) {
+        if(key == "name") continue;
+        item = new UIMenuItem(
+            data[key].name,
+            ". ~g~Tip:~w~ Those vehicle are temporary and will disapear at each restart!"
+        );
+        item.SetRightLabel("~g~$" + data[key].price);
+        ui.AddItem(item);
+    }  
+    ui.IndexChange.on( (index) => {
+        mp.players.local.vehicleShop.model = data[Object.keys(data)[index+1]].hash;
+        mp.players.local.vehicleShop.price = data[Object.keys(data)[index+1]].price;
+    });
+    ui.ItemSelect.on( (item) => {
+        mp.events.callRemote("buyVehicleShop", carShopType, JSON.stringify({"model": mp.players.local.vehicleShop.model, "price": mp.players.local.vehicleShop.price}));
+    });
+    ui.MenuClose.on( () => {
+        if(carShopData && carShopData.length > 1) { 
+            return carShopMenu();
+        } else if(carShopData) {
+            ui = null;
+            exitCarShop();
+        }
+    });
+}
+
+function exitCarShop() {    
+    carShopData = null;
+    carShopType = null;
+    carShopPos = null;
+    mp.players.local.vehicleShop.destroy();
+    delete mp.players.local.vehicleShop;
+    if(ui) ui.Close();
+    cam.setActive(false);
+    mp.game.cam.renderScriptCams(false, true, 0, true, true);
+    cam.destroy();
+    cam = null; 
+    mp.events.callRemote("exitVehicleShop");
+}
+
+
+mp.events.add({
+    "showVehicleShop": (cars, carPos, type) => {
+        cars = JSON.parse(cars);
+        pos = JSON.parse(carPos);
+
+        carShopType = type;
+        carShopData = cars;
+        carShopPos = pos;
+
+        player = mp.players.local; 
+        player.vehicleShop = mp.vehicles.new(cars[0][Object.keys(cars[0])[1]].hash, new mp.Vector3(pos.Position.x, pos.Position.y, pos.Position.z), {
+            numberPlate: "SHOP",
+            dimension: player.dimension,
+            heading: pos.Position.rz,
+            engine: true
+        });
+
+        cam = mp.cameras.new("Camera", {x:pos.Position.x-5, y: pos.Position.y-5, z: pos.Position.z+2}, {x: 0, y: 0, z: pos.Position.rz}, 50);
+        cam.setActive(true);
+        cam.pointAt(player.vehicleShop.handle, 0,0,0, true) ;
+        mp.game.cam.renderScriptCams(true, false, 0, true, false);
+        carShopMenu()   
+    },
+    "exitVehicleShop": () => {
+        exitCarShop();
+    }
+})
